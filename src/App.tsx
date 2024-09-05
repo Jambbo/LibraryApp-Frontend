27ed1ce7dae960d1fdbc5c419ede6f77252837f6
Gleb <chibichyangleb@gmail.com>
2024-09-05 09:14:03 +0100

    added ExploreTopBooks component for home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import './App.css';
+import { ExploreTopBooks } from './layouts/NavbarAndFooter/HomePage/ExploreTopBooks';
 
 function App() {
   return (
+    <div>
       <Navbar/>
+      <ExploreTopBooks/>
+    </div>
   );
 }
 
