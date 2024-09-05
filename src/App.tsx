742fde37b936c62c59e973e2ae6593f3da4ae442
Gleb <chibichyangleb@gmail.com>
2024-09-05 10:02:49 +0100

    Carousel added for HomePage for Desktop and Mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import './App.css';
 import { ExploreTopBooks } from './layouts/NavbarAndFooter/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/NavbarAndFooter/HomePage/Carousel';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <ExploreTopBooks/>
+      <Carousel/>
     </div>
   );
 }
