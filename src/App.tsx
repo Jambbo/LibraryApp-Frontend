a71ee453d540e81eb77513dfd80ccabc312189e6
Gleb <chibgleb99@gmail.com>
2024-09-06 09:47:36 +0100

    fixed bug with images on mob. version

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import './App.css';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { SearchBook } from './layouts/SearchBooksPage/components/SearchBook';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
 
 export const App = () => {
   return (
     <div>
         <Navbar/>
-        <HomePage/>
+        {/* <HomePage/> */}
+        <SearchBooksPage/>
         <Footer/>
     </div>
   );
