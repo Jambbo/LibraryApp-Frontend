7419be621870f163716df2285e8aa2dbae64da9a
Gleb <chibgleb99@gmail.com>
2024-09-10 11:12:30 +0100

    added react routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,30 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { SearchBook } from './layouts/SearchBooksPage/components/SearchBook';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 
 export const App = () => {
   return (
     <div>
-        <Navbar/>
-        {/* <HomePage/> */}
-        <SearchBooksPage/>
-        <Footer/>
+      <Navbar />
+      <Switch>
+
+        <Route path='/' exact> {/* exact keyword says if the path is exactly '/' then redirect, not if it is '/smthing' */ }
+          <Redirect to='/home' />
+        </Route>
+
+        <Route path='/home'>
+          <HomePage />
+        </Route>
+
+        <Route path='/search'>
+          <SearchBooksPage />
+        </Route>
+
+
+      </Switch>
+      <Footer />
     </div>
   );
 }
