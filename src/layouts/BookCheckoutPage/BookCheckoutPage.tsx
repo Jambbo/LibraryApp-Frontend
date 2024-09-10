1805ce35781bd410e7e9831dfd32368abe297aea
Gleb <chibgleb99@gmail.com>
2024-09-10 13:33:46 +0100

    implemented book display using useEffect hook

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,13 +1,62 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BookModel } from "../../models/BookModel";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
 
 export const BookCheckoutPage = ()=> {
 
     const[book, setBook] = useState<BookModel>();
-    const[isLoadingBook, setIsLoadingBook] = useState(true);
+    const[isLoading, setIsLoading] = useState(true);
     const[httpError, setHttpError] = useState(null);
     
     const bookId = (window.location.pathname).split('/')[2];
+
+    useEffect(() => {
+        const fetchBook = async () => {
+            const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
+
+
+            const response = await fetch(baseUrl);
+
+            if (!response.ok) {
+                throw new Error(`Something went wrong!`);
+            }
+
+            const responseData = await response.json();
+
+            const loadedBook: BookModel = {
+                id: responseData.id,
+                title: responseData.title,
+                author: responseData.author,
+                description: responseData.description,
+                copies: responseData.copies,
+                copiesAvailable: responseData.copiesAvailable,
+                category: responseData.category,
+                imgDataId: responseData.imgDataId,
+            };
+
+            setBook(loadedBook);
+            setIsLoading(false);
+
+        };
+        fetchBook().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+    }, []);
+
+    if (isLoading) {
+        return (
+            <SpinnerLoading/>
+        )
+    }
+
+    if (httpError) {
+        return (
+            <div className="container m-5">
+                <p>{httpError}</p>
+            </div>
+        );
+    }
 
 
     return(
