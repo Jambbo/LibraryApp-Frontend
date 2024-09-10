7144e510d3119c4f8ab9d0c2607a397a7b11932c
Gleb <chibgleb99@gmail.com>
2024-09-10 11:43:33 +0100

    implemented redirecting using Link

--- src/layouts/NavbarAndFooter/Footer.tsx
+++ src/layouts/NavbarAndFooter/Footer.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 export const Footer = ()=>{
     return (
@@ -7,14 +8,14 @@ export const Footer = ()=>{
                 <p className="col-md-4 mb-0 text-white">© Whassup Library App, Inc </p>
                 <ul className="nav navbar-dark col-md-4 justify-content-end">
                     <li className="nav-item">
-                        <a href="#" className="nav-link px-2 text-white">
+                        <Link className="nav-link px-2 text-white" to="/home">
                             Home
-                        </a>
+                        </Link>
                     </li> 
                     <li className="nav-item">
-                        <a href="#" className="nav-link px-2 text-white">
+                        <Link className="nav-link px-2 text-white" to="/search">
                             Seach Books
-                        </a>
+                        </Link>
                     </li>
                 </ul>
             </footer>
